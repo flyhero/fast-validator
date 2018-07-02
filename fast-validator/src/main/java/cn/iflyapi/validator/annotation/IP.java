3eb97602a7cb653df7777ddc7504152bfef40910
qfwang <wangqingfei@dfocuspace.com>
2018-07-02 21:26:02 +0800

    feat:增加IP注解和handler类

---
+++ fast-validator/src/main/java/cn/iflyapi/validator/annotation/IP.java
@@ -0,0 +1,9 @@
+package cn.iflyapi.validator.annotation;
+
+/**
+ * @author: qfwang
+ * @date: 2018-07-02 下午9:25
+ */
+public @interface IP {
+    String value();
+}
---
+++ fast-validator/src/main/java/cn/iflyapi/validator/core/ValidatorHandler.java
@@ -0,0 +1,8 @@
+package cn.iflyapi.validator.core;
+
+/**
+ * @author: qfwang
+ * @date: 2018-07-02 下午9:24
+ */
+public class ValidatorHandler {
+}
