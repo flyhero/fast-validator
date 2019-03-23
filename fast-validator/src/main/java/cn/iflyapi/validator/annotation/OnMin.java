75d692b215cb8530882d765e7810fc6df5d969c7
flyhero <qfwang666@163.com>
2019-03-23 20:24:53 +0800

    feat:增加参数大小注解

---
+++ fast-validator/src/main/java/cn/iflyapi/validator/annotation/OnMin.java
@@ -0,0 +1,14 @@
+package cn.iflyapi.validator.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author flyhero
+ * @date 2019-03-23 8:19 PM
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface OnMin {
+    long value() default 0;
+}
