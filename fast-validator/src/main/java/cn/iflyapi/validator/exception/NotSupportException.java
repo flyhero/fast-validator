e1df7bfe1afa8d39f4da4a9f1a649c44cc878a2c
flyhero <qfwang666@163.com>
2019-04-14 20:45:38 +0800

    feat:增加不支持类型的异常类

---
+++ fast-validator/src/main/java/cn/iflyapi/validator/exception/NotSupportException.java
@@ -0,0 +1,11 @@
+package cn.iflyapi.validator.exception;
+
+/**
+ * @author flyhero
+ * @date 2018-05-22 下午2:44
+ */
+public class NotSupportException extends RuntimeException {
+    public NotSupportException(String message) {
+        super(message);
+    }
+}
